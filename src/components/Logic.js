9dbbf703d4536f97d43499753886b64efc3d3f66
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-09-12 18:01:33 -0600

    Sixth Commit

--- src/components/Logic.js
+++ src/components/Logic.js
@@ -1,11 +1,13 @@
 import {  Button } from 'reactstrap';
 import style from '../shared/StudentList.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SelectAllQuestions } from './Navigation';
 import Questions from '../components/Questions';
 import { AnswersA,AnswersB,AnswersC,AnswersD } from "./Answers";
 import TheSideBar from './TheSideBar';
 import Music from './Music';
+import useSound from 'use-sound';
+import correct_Ans from '../sounds/correct answer.mp3';
 
 const Logic = () =>{
     console.log('im in');
@@ -13,6 +15,21 @@ const Logic = () =>{
     const [question_new,setQuestion_new] = useState(SelectAllQuestions);
     const [incorrect,setIncorrect] = useState(false);
     const [bank,setBank] = useState(0);
+    const [counter, setCounter] = useState(60);
+
+    const [correct_ans, {stop}] = useSound(correct_Ans, {
+        volume: 0.5,
+       });
+
+    //Responsible for setting up the timer if the timer is at 0 the game ends
+    useEffect(() => {
+        const timer =
+            counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+            if(counter === 0){
+                setIncorrect(0);
+            }
+            return () => {clearInterval(timer)};
+            },[counter] );
 
     const setDigits = [100,200,300,500,1000,2000,4000,8000,16000,32000,64000,125000,250000,500000,1000000]
     
@@ -26,24 +43,32 @@ const Logic = () =>{
             console.log('youve answered question a correctly')
             setQuestionNumber(the_progress+1);
             setBank(setDigits[(the_progress+1) -2]);
+            setCounter(60);
+            correct_ans();
             console.log(bank);
         }
         else if (ans === 'b' && ans === question_new[the_progress].ans){
             console.log('youve answered question b correctly')
             setQuestionNumber(the_progress+1);
             setBank(setDigits[(the_progress+1) -2]);
+            setCounter(60);
+            correct_ans();
             console.log(bank);
         }
         else if (ans === 'c' && ans === question_new[the_progress].ans){
             console.log('youve answered question c correctly')
             setQuestionNumber(the_progress+1);
             setBank(setDigits[(the_progress+1) -2]);
+            setCounter(60);
+            correct_ans();
             console.log(bank);
         }
         else if (ans === 'd' && ans === question_new[the_progress].ans){
             console.log('youve answered question d correctly')
             setQuestionNumber(the_progress+1);
             setBank(setDigits[(the_progress+1) -2]);
+            setCounter(60);
+            correct_ans();
             console.log(bank);
         }
         else{
@@ -60,6 +85,7 @@ const Logic = () =>{
     <button className={style.answer3} onClick={(event) => {getAnswers('c',questionNumber,event)} }><AnswersC  prop={question_new} prop2={questionNumber}></AnswersC></button>
     <button className={style.answer4} onClick={(event) => {getAnswers('d',questionNumber,event)} }><AnswersD  prop={question_new} prop2={questionNumber}></AnswersD></button>
     <div className={style.sideBar}><TheSideBar prop={bank} prop2={questionNumber}/></div>,
+    <div className={style.my_counter}>{counter}</div>,
     <Music prop={bank}/>
     </>
     
